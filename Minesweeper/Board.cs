a9520eca3bcc71f99d2e60ebe3c91bb81d899326
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-11-30 13:05:55 -0800

    Created Board Class

---
+++ Minesweeper/Board.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Minesweeper
+{
+    class Board
+    {
+        private enum TILES : int { WALL = -2, MINE = -1};
+        private int length, width;
+        private int[,] board_rep;
+
+        public Board(int l, int w)
+        {
+            //Collect board dims
+            Debug.Assert(l > 0 && w > 0);
+            length = l;
+            width = w;
+
+            //Set up board with borders
+            board_rep = new int[length + 2, width + 2];
+            for (int i = 0; i < length + 2; i++)
+            {
+                board_rep[i, 0] = (int)TILES.WALL;
+                board_rep[i, width + 1] = (int)TILES.WALL;
+            }
+            for (int i = 0; i < width + 2; i++)
+            {
+                board_rep[0, i] = (int)TILES.WALL;
+                board_rep[length + 1, i] = (int)TILES.WALL;
+            }
+
+            for (int i = 1; i < width + 1; i++)
+            {
+                for (int j = 1; j < length + 1; j++)
+                {
+                    board_rep[j, i] = 0;
+                }
+            }
+            populate_board();
+        }
+        private void populate_board()
+        {
+            //Place mines and numbers
+        }
+        public void print_board()
+        {
+            for (int i = 1; i < width + 1; i++)
+            {
+                for (int j = 1; j < length + 1; j++)
+                {
+                    Console.Write((board_rep[j, i]).ToString() + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+    }
+}
--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -16,34 +16,13 @@ namespace Minesweeper
             string lstr = Console.ReadLine();
             Console.Write("Enter width: ");
             string wstr = Console.ReadLine();
-            Int32.TryParse(lstr, out board_dims[1]);
-            Int32.TryParse(wstr, out board_dims[0]);
+            Int32.TryParse(lstr, out board_dims[0]);
+            Int32.TryParse(wstr, out board_dims[1]);
 
-            // Populating board
-            int[,] board = new int[board_dims[0], board_dims[1]];
-            for(int i = 0; i < board_dims[0]; i++)
-            {
-                for(int j = 0; j < board_dims[1]; j++)
-                {
-                    board[i, j] = i + j;
-                }
-            }
-
-            // Printing board
-            printBoard(board, board_dims);
+            Board my_board = new Board(board_dims[0], board_dims[1]);
+            my_board.print_board();
 
             Console.ReadKey();
-        }
-        static void printBoard(int[,] b, int[] dims)
-        {
-            for (int i = 0; i < dims[0]; i++)
-            {
-                for (int j = 0; j < dims[1]; j++)
-                {
-                    Console.Write((b[i, j]).ToString() + ' ');
-                }
-                Console.Write('\n');
-            }
         }
     }
 }
